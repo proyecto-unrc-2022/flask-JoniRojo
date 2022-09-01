17a0ddc0ffa1216208bae1ca2dfff0d14e2b11dc
Jonathan Rojo <jonathanrojo2002@gmail.com>
2022-09-01 01:02:56 -0300

    Support POST operation to add a new user

--- features/steps/user.py
+++ features/steps/user.py
@@ -1,4 +1,6 @@
 import json
+from multiprocessing import context
+from tkinter import W
 from behave import *
 from application import USERS
 
@@ -19,3 +21,35 @@ def step_impl(context):
 def step_impl(context):
     # assert context.table[0].cells[0] in context.page.text
     assert "Jason Bourne" in context.page.text
+
+#-----------------------------
+
+@given('the user "marty" doesnt exist')
+def step_impl(context):
+    assert 'marty' not in USERS
+
+@when('I store the costumer "marty"')
+def step_impl(context):
+    new_user = { 'username' : 'marty',
+                 'name' : 'San Martin'
+               }
+    context.page = context.client.post('/users', data = new_user)
+    assert context.page
+
+@then(u'I should get a \'201\' response')
+def step_impl(context):
+    assert context.page.status_code is 201
+
+@then('"marty" is in the database')
+def step_impl(context):
+    assert 'marty' in USERS
+
+#-----------------------------
+
+#@given('that I have users are in the system')
+#def step_impl(context):
+#    USERS
+
+#@when('I retrieve all the customers')
+#def step_impl(context):
+#    context.page = context.client.get('') ... ??
--- features/user.feature
+++ features/user.feature
@@ -7,3 +7,17 @@ Feature: Handle storing, retrieving and deleting customer details # test/feature
     And the following user details are returned:
       | name        |
       | Jason Borne |
+
+  Scenario: Add a new user
+    Given the user "marty" doesnt exist
+    When I store the custumer "marty"
+    Then I should get a '201' response
+    And "marty" is in the database
+
+  #Scenario: Get operation to list all system users
+   # Given that I have users are in the system
+    #When I retrieve all the customers
+    #Then I should get a '200' response
+    #And the following user details are returned:
+    #  | name        |
+    #  | Jason Borne |  ... ??
